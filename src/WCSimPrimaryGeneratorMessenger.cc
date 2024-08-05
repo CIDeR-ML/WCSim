1b2bac8efc6f5e06d90e0e8c8cf2ab0c8ec6faeb
Junjie Xia <junjie@sdfiana008.sdf.slac.stanford.edu>
2024-08-05 13:29:32 -0700

    remove G4DoubleAndUnit

--- src/WCSimPrimaryGeneratorMessenger.cc
+++ src/WCSimPrimaryGeneratorMessenger.cc
@@ -206,16 +206,15 @@ WCSimPrimaryGeneratorMessenger::WCSimPrimaryGeneratorMessenger(WCSimPrimaryGener
   nGammaCmd->SetParameterName("nGamma",true);
   nGammaCmd->SetDefaultValue(1);
 
-  r0Cmd = new G4UIcmdWithADoubleAndUnit("/mygen/r0_Vox",this);
+  r0Cmd = new G4UIcmdWithADouble("/mygen/r0_Vox",this);
   r0Cmd->SetGuidance("Set the radius lower limit of the voxel in which the gammas are generated");
   r0Cmd->SetGuidance("[usage] /mygen/r0_Vox r0 unit");
   r0Cmd->SetGuidance(" r0 : r0 (where r0 is given in json) ");
   r0Cmd->SetRange("r0>=0");
-  r0Cmd->SetUnitCategory("Distance");
   r0Cmd->SetParameterName("r0",true);
   r0Cmd->SetDefaultValue(0.0*cm);
 
-  r1Cmd = new G4UIcmdWithADoubleAndUnit("/mygen/r1_Vox",this);
+  r1Cmd = new G4UIcmdWithADouble("/mygen/r1_Vox",this);
   r1Cmd->SetGuidance("Set the radius upper limit of the voxel in which the gammas are generated");
   r1Cmd->SetGuidance("[usage] /mygen/r1_Vox r1 unit");
   r1Cmd->SetGuidance(" r1 : r1 (where r1 is given in json) ");
@@ -224,7 +223,7 @@ WCSimPrimaryGeneratorMessenger::WCSimPrimaryGeneratorMessenger(WCSimPrimaryGener
   r1Cmd->SetParameterName("r1",true);
   r1Cmd->SetDefaultValue(1.0*cm);
 
-  z0Cmd = new G4UIcmdWithADoubleAndUnit("/mygen/z0_Vox",this);
+  z0Cmd = new G4UIcmdWithADouble("/mygen/z0_Vox",this);
   z0Cmd->SetGuidance("Set the z lower limit of the voxel in which the gammas are generated");
   z0Cmd->SetGuidance("[usage] /mygen/z0_Vox z0 unit");
   z0Cmd->SetGuidance(" z0 : z0 (where z0 is given in json) ");
@@ -232,7 +231,7 @@ WCSimPrimaryGeneratorMessenger::WCSimPrimaryGeneratorMessenger(WCSimPrimaryGener
   z0Cmd->SetParameterName("z0",true);
   z0Cmd->SetDefaultValue(0.0*cm);
 
-  z1Cmd = new G4UIcmdWithADoubleAndUnit("/mygen/z1_Vox",this);
+  z1Cmd = new G4UIcmdWithADouble("/mygen/z1_Vox",this);
   z1Cmd->SetGuidance("Set the z upper limit of the voxel in which the gammas are generated");
   z1Cmd->SetGuidance("[usage] /mygen/z1_Vox z1 unit");
   z1Cmd->SetGuidance(" z1 : z1 (where z1 is given in json) ");
